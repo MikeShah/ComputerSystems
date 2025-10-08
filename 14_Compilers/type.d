ffc42c3c9bf95b2c047e072093731bba445ddc82
Mike <mike@system76-pc.localdomain>
2025-10-08 11:52:30 -0400

    compiler examples

---
+++ 14_Compilers/type.d
@@ -0,0 +1,5 @@
+void main(){
+
+	float f = 3.14f;
+	int x = f; 	// int x = cast(int)f;
+}				// would work.
---
+++ 14_Compilers/type.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let my_float: f32 = 3.14;
+    let my_int: i32 = my_float;
+	// Must cast as show below
+//    let my_int : i32 = my_float as i32;
+}
