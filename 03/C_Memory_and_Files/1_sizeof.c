9c3e5d2ca735b967be0fa8454495590ed924702d
Mike <mike@system76-pc.localdomain>
2026-01-14 11:20:19 -0500

    restructure

--- 03/C_Memory_and_Files/1_sizeof.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-
-    printf("char, float: %lu,%lu\n",sizeof(char),sizeof(float));
-
-    return 0;
-}
--- 03/C_Memory_and_Files/mem.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-
-    int a=5;
-    printf("a's address is: %p\n",(int*)&a);
-
-    return 0;
-}
