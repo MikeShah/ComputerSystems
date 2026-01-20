4a4cdaf16cdc09b1a64bcaa9e448318f1f8f2f91
MikeShah <mshah.475@gmail.com>
2026-01-19 21:00:09 -0500

    completed example

--- simd/memcmp.c
+++ simd/memcmp.c
@@ -10,8 +10,15 @@
 // This example uses the 'memcmp' function. It is similar to 'strcmp' in
 // the <string.h> header, but it does not use the NULL terminator.
 // Rather it treats two 'buffers' as unsigned char (or uint8_t) data and compares each byte.
+//
+// Note: You can improve the benchmarking or just comment out each example to
+//       using the '#if 0' or '#if 1' for the larger benchmarks to time the runs.
+//
+//       gcc memcmp.c -o prog && time ./prog
+
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h> // for C standard library memcmp
 // int memcmp(const void s1[.n], const void s2[.n], size_t n);
 
@@ -61,6 +68,7 @@ int memcmp_uint64_t(const void* s1, const void* s2, size_t n){
 
 int main(){
 
+#if 0
   // Standard library example
   unsigned char buf1[] = {1,2,3,4,5,6,7,8,9,10};
   unsigned char buf2[] = {0,1,2,3,4,5,6,7,8,9};
@@ -74,10 +82,38 @@ int main(){
   printf("memcmp_first_try buf2 and buf1: %d\n",memcmp_first_try(buf2,buf1,sizeof(buf1)));
   printf("memcmp_first_try buf3 and buf1: %d\n",memcmp_first_try(buf3,buf1,sizeof(buf1)));
 
-
   printf("memcmp_uint64_t buf1 and buf2: %d\n",memcmp_uint64_t(buf1,buf2,sizeof(buf1)));
   printf("memcmp_uint64_t buf2 and buf1: %d\n",memcmp_uint64_t(buf2,buf1,sizeof(buf1)));
   printf("memcmp_uint64_t buf3 and buf1: %d\n",memcmp_uint64_t(buf3,buf1,sizeof(buf1)));
+#endif
+
+  const long BIG=100000000;
+  unsigned char* buf4 = (unsigned char*)malloc(BIG*sizeof(unsigned char));
+  unsigned char* buf5 = (unsigned char*)malloc(BIG*sizeof(unsigned char));
+  unsigned char* buf6 = (unsigned char*)malloc(BIG*sizeof(unsigned char));
+  for(size_t i=0; i < BIG; i++){
+    buf4[i] = i;
+    buf5[i] = i+1;
+    buf6[i] = i;
+  }
+
+#if 0
+  printf("BIG memcmp buf4 and buf5: %d\n",memcmp(buf4,buf5,BIG));
+  printf("BIG memcmp buf5 and buf4: %d\n",memcmp(buf5,buf4,BIG));
+  printf("BIG memcmp buf4 and buf6: %d\n",memcmp(buf4,buf6,BIG));
+#endif
+
+#if 0
+  printf("BIG memcmp_first_try buf4 and buf5: %d\n",memcmp_first_try(buf4,buf5,BIG));
+  printf("BIG memcmp_first_try buf5 and buf4: %d\n",memcmp_first_try(buf5,buf4,BIG));
+  printf("BIG memcmp_first_try buf4 and buf6: %d\n",memcmp_first_try(buf4,buf6,BIG));
+#endif
+
+#if 1
+  printf("BIG memcmp_uint64_t buf4 and buf5: %d\n",memcmp_uint64_t(buf4,buf5,BIG));
+  printf("BIG memcmp_uint64_t buf5 and buf4: %d\n",memcmp_uint64_t(buf5,buf4,BIG));
+  printf("BIG memcmp_uint64_t buf4 and buf6: %d\n",memcmp_uint64_t(buf4,buf6,BIG));
+#endif
 
   return 0;
 }
