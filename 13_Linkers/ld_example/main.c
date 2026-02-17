7100098a528d4434ca10bd5fc40fa2f106c382ec
MikeShah <mshah.475@gmail.com>
2026-02-17 15:45:13 -0500

    updates

--- 13_Linkers/ld_example/main.c
+++ 13_Linkers/ld_example/main.c
@@ -6,5 +6,14 @@
 
 int main(){
 
+  int array[] = {1,2,3,4};
+
+  // We can compute the 'sizeof' a stack allocated array.
+  // This *WILL NOT* work however with a 'heap allocated'
+  // array -sum(array,sizeof(array)/sizeof(int))- we will need to track the size manually.
+  int total = sum(array,sizeof(array)/sizeof(int));
+
+  printf("total is: %d\n",total);
+
 	return 0;
 }
--- 13_Linkers/test1.c
+++ 13_Linkers/test1.c
@@ -1,11 +1,20 @@
 // @file: test1.c
-// Try running this file through the compiler and emitting the assembly
-// with: 
+//
+// Try running this file through the compiler and emitting the assembly with: 
 // gcc -c -g -O -Wa,-alh,-L test1.c
+//
+// Try compiling this example and then using the 'linker'
+// 1.) First build the object file
+//     gcc -c main.c
+// 2.) Then setup the linker to link machine code with the C-runtime (which has a _start) and allow us to
+//     run our program.
+//     ld -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o /usr/lib/x86_64-linux-gnu/crtn.o test1.o -o prog -lc
+#include <stdio.h>
 
 int main(){
 
 	int a = 72;
+  printf("a is %d\n",a);
 
 	return 0;
 }
