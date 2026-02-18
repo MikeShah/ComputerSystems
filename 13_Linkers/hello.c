f8b4b359171e05d8ff0e4e3b5ccfb56f0a7ae809
Mike <mike@system76-pc.localdomain>
2026-02-18 15:35:31 -0500

    hello

---
+++ 13_Linkers/hello.c
@@ -0,0 +1,11 @@
+// @file: hello.c
+// Try compiling this and then observing using 'ldd' the
+// libc standard library is linked in.
+#include <stdio.h>
+
+int main(){
+
+  printf("hello\n");
+
+  return 0;
+}
